8e31dcc6cdc9206ac44332ea037124349621173f
Daniel Nichter <daniel@percona.com>
2011-08-19 11:20:40 -0600

    Fix typos in pt-summary tests from r85.

--- t/pt-summary/parse_raid_controller_lspci.sh
+++ t/pt-summary/parse_raid_controller_lspci.sh
@@ -3,35 +3,35 @@
 TEST=5
 
 TEST_NAME="lspci-001.txt"
-<<EOF > $TMPDIR/expected
+cat <<EOF > $TMPDIR/expected
 Fusion-MPT SAS
 EOF
 parse_raid_controller_lspci samples/lspci-001.txt > $TMPDIR/got
 no_diff $TMPDIR/got $TMPDIR/expected
 
 TEST_NAME="lspci-002.txt"
-<<EOF > $TMPDIR/expected
+cat <<EOF > $TMPDIR/expected
 LSI Logic Unknown
 EOF
 parse_raid_controller_lspci samples/lspci-002.txt > $TMPDIR/got
 no_diff $TMPDIR/got $TMPDIR/expected
 
 TEST_NAME="lspci-003.txt"
-<<EOF > $TMPDIR/expected
+cat <<EOF > $TMPDIR/expected
 AACRAID
 EOF
 parse_raid_controller_lspci samples/lspci-003.txt > $TMPDIR/got
 no_diff $TMPDIR/got $TMPDIR/expected
 
 TEST_NAME="lspci-004.txt"
-<<EOF > $TMPDIR/expected
+cat <<EOF > $TMPDIR/expected
 LSI Logic MegaRAID SAS
 EOF
 parse_raid_controller_lspci samples/lspci-004.txt > $TMPDIR/got
 no_diff $TMPDIR/got $TMPDIR/expected
 
 TEST_NAME="lspci-006.txt"
-<<EOF > $TMPDIR/expected
+cat <<EOF > $TMPDIR/expected
 HP Smart Array
 EOF
 parse_raid_controller_lspci samples/lspci-006.txt > $TMPDIR/got
