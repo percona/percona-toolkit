d3ad8c84c078695155ceb3560b20b0d8e4e226f9
Daniel Nichter <daniel@percona.com>
2012-03-24 12:47:08 -0600

    Do not ever create --new-table.  Rewrite basics.t.  Add more debug statements.

--- t/pt-online-schema-change/samples/basic_with_fks.sql
+++ t/pt-online-schema-change/samples/basic_with_fks.sql
@@ -1,6 +1,8 @@
-DROP DATABASE IF EXISTS `mkosc`;
-CREATE DATABASE `mkosc`;
-USE `mkosc`;
+DROP DATABASE IF EXISTS pt_osc;
+CREATE DATABASE pt_osc;
+USE pt_osc;
+
+SET foreign_key_checks=0;
 
 CREATE TABLE `country` (
   `country_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -29,3 +31,26 @@ CREATE TABLE `address` (
   KEY `idx_fk_city_id` (`city_id`),
   CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB;
+
+INSERT INTO pt_osc.country VALUES
+  (1, 'Canada', null),
+  (2, 'USA',    null),
+  (3, 'Mexico', null),
+  (4, 'France', null),
+  (5, 'Spain',  null);
+
+INSERT INTO pt_osc.city VALUES
+  (null, 'Montréal', 1, null),
+  (null, 'New York', 2, null),
+  (null, 'Durango',  3, null),
+  (null, 'Paris',    4, null),
+  (null, 'Madrid',   5, null);
+
+INSERT INTO pt_osc.address VALUES
+  (null, 'addy 1', 1, '10000', null),
+  (null, 'addy 2', 2, '20000', null),
+  (null, 'addy 3', 3, '30000', null),
+  (null, 'addy 4', 4, '40000', null),
+  (null, 'addy 5', 5, '50000', null);
+
+SET foreign_key_checks=1;
