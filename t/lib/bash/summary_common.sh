3d1e7dafee67f57f6308095e3024541fa8e1d366
Brian Fraser fraserb@gmail.com <>
2012-03-28 15:59:16 -0300

    More tests for get_var

--- t/lib/bash/summary_common.sh
+++ t/lib/bash/summary_common.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-plan 3
+plan 8
 
 TMPDIR="$TEST_TMPDIR"
 PATH="$PATH:$PERCONA_TOOLKIT_SANDBOX/bin"
@@ -26,3 +26,35 @@ is \
    "$(get_var "test::1" "$p")" \
    "abcdef"                \
    "get_var works if the 'key' has colons"
+
+
+echo "1234/567    qwerty" > "$p"
+is \
+   "$(get_var "1234/567" "$p")" \
+   "qwerty"                \
+   "get_var works if the 'key' has a dash in it"
+
+
+echo ".*    some_new_value" >> "$p"
+is \
+   "$(get_var ".*" "$p")" \
+   "some_new_value"                \
+   "get_var treats the variable as a literal, not a regex"
+
+if get_var "definitely_does_not_exist" "$p" 1>/dev/null ; then
+   pass "get_var always returns true, even for variables that don't exist"
+else
+   fail "get_var should always return true"
+fi
+
+samples="$PERCONA_TOOLKIT_BRANCH/t/pt-mysql-summary/samples"
+
+is \
+   "$(get_var "table_open_cache" "$samples/temp002/mysql-variables")" \
+   "400"                \
+   "get_var works on a variables dump"
+
+is \
+   "$(get_var "Open_tables" "$samples/temp002/mysql-status")" \
+   "40"                \
+   "get_var works on a status dump"
