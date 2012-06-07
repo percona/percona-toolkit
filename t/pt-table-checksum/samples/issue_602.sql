a015bf05e80d9d8e0618898798f130a2d04f46c1
Baron Schwartz <baron@xaprb.com>
2012-06-07 14:53:47 -0400

    Some fixes for ANSI SQL quotes

--- t/pt-table-checksum/samples/issue_602.sql
+++ t/pt-table-checksum/samples/issue_602.sql
@@ -7,16 +7,16 @@ create table t (
    b datetime not null,
    key (b)
 );
-insert into t VALUES (1, "2010-05-09 00:00:00");
-insert into t VALUES (2, "2010-05-08 00:00:00");
-insert into t VALUES (3, "2010-05-07 00:00:00");
-insert into t VALUES (4, "2010-05-06 00:00:00");
-insert into t VALUES (5, "2010-05-05 00:00:00");
-insert into t VALUES (6, "2010-05-04 00:00:00");
-insert into t VALUES (7, "2010-05-03 00:00:00");
-insert into t VALUES (8, "2010-05-02 00:00:00");
-insert into t VALUES (9, "2010-05-01 00:00:00");
-insert into t VALUES (10, "2010-04-30 00:00:00");
+insert into t VALUES (1, '2010-05-09 00:00:00');
+insert into t VALUES (2, '2010-05-08 00:00:00');
+insert into t VALUES (3, '2010-05-07 00:00:00');
+insert into t VALUES (4, '2010-05-06 00:00:00');
+insert into t VALUES (5, '2010-05-05 00:00:00');
+insert into t VALUES (6, '2010-05-04 00:00:00');
+insert into t VALUES (7, '2010-05-03 00:00:00');
+insert into t VALUES (8, '2010-05-02 00:00:00');
+insert into t VALUES (9, '2010-05-01 00:00:00');
+insert into t VALUES (10, '2010-04-30 00:00:00');
 
 -- invalid datetime
 insert into t VALUES (11, '2010-00-09 00:00:00' );
@@ -29,11 +29,11 @@ create table t2 (
    b datetime not null,
    key (b)
 );
-insert into t2 VALUES (1, "2010-00-01 00:00:01");
-insert into t2 VALUES (2, "2010-00-02 00:00:02");
-insert into t2 VALUES (3, "2010-00-03 00:00:03");
-insert into t2 VALUES (4, "2010-00-04 00:00:04");
-insert into t2 VALUES (5, "2010-00-05 00:00:05");
-insert into t2 VALUES (6, "2010-00-06 00:00:06");
-insert into t2 VALUES (7, "2010-01-07 00:00:07");
-insert into t2 VALUES (7, "2010-01-08 00:00:08");
+insert into t2 VALUES (1, '2010-00-01 00:00:01');
+insert into t2 VALUES (2, '2010-00-02 00:00:02');
+insert into t2 VALUES (3, '2010-00-03 00:00:03');
+insert into t2 VALUES (4, '2010-00-04 00:00:04');
+insert into t2 VALUES (5, '2010-00-05 00:00:05');
+insert into t2 VALUES (6, '2010-00-06 00:00:06');
+insert into t2 VALUES (7, '2010-01-07 00:00:07');
+insert into t2 VALUES (7, '2010-01-08 00:00:08');
