5b9932ca2850a541a40ee5328e877f9006184c6a
Brian Fraser <brian.fraser@percona.com>
2013-01-31 19:51:38 -0300

    Updated some files that still used the old ReportFormatter

--- lib/CompareQueryTimes.pm
+++ lib/CompareQueryTimes.pm
@@ -298,7 +298,7 @@ sub _report_diff_big {
    return unless keys %{$self->{diffs}->{big}};
 
    my $report = new ReportFormatter();
-   $report->set_title('Big query time differences');
+   $report->title('Big query time differences');
    my $hostno = 0;
    $report->set_columns(
       $args{query_id_col},
@@ -347,7 +347,7 @@ sub _report_diff_in_bucket {
    return unless keys %{$self->{diffs}->{in_bucket}};
 
    my $report = new ReportFormatter();
-   $report->set_title('Significant query time differences');
+   $report->title('Significant query time differences');
    my $hostno = 0;
    $report->set_columns(
       $args{query_id_col},
