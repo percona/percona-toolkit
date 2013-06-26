61a077097277bb7c7762ac8c2a6483eaff6f3f1d
Daniel Nichter <daniel@percona.com>
2013-06-26 12:07:06 -0700

    Add failing test.

---
+++ t/pt-upgrade/samples/007/tables.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+USE test;
+CREATE TABLE t (
+  id         int(10) NOT NULL AUTO_INCREMENT,
+  username   varchar(8) default NULL,
+  PRIMARY KEY  (`id`)
+);
+INSERT INTO t VALUES
+  (null,   'a'),
+  (null,  null);
