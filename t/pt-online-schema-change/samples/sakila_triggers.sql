328818564306d60d665d96034530ea1318f4f120
Carlos Salguero <carlos.salguero@percona.com>
2016-08-04 17:36:40 -0300

    Revert "Merge pull request #107 from percona/bug-984057"
    This reverts commit 417b24299cc7f0641e622285ed0de93003fa3a70, reversing
    changes made to 0b62ba26af1dc1506f3c4a9c573a642a0490dfc2.

--- t/pt-online-schema-change/samples/sakila_triggers.sql
+++
@@ -1,9 +0,0 @@
-USE sakila;
-CREATE TRIGGER triggers_migration_test
-BEFORE
--- a comment here
-  INSERT ON film 
--- just to make things harder
-  FOR EACH ROW SET NEW.length = 60  
--- for pt_osc
-;
