b6bec3965a5a609104a1d54746b2b523c5843c07
Carlos Salguero <carlos.salguero@percona.com>
2017-07-27 11:28:46 -0300

    PT-181 online-schema-change not in sync with modules
    - Modules updated
    - Improved tests

--- t/pt-online-schema-change/samples/slave_lag.sql
+++ t/pt-online-schema-change/samples/slave_lag.sql
@@ -37,7 +37,7 @@ DROP TABLE IF EXISTS `pt178`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pt178` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  f1 VARCHAR(30) NULL,
+  f1 VARCHAR(30) DEFAULT '',
   f2 BIGINT(11) DEFAULT 0,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB;
