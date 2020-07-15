95259c3f096b551db6237974269a372d3840c1b8
Carlos <cfsalguero@gmail.com>
2020-07-14 22:52:16 -0300

    PMM-6256 Updated ExplainCmd for MongoDB
    In order to make explain for MongoDB work, we need to remove the "$db"
    field from the explain command generated in proto.system.profile since
    it is a duplicated field that triggers a MongoDB error.

--- src/go/mongolib/proto/system.profile.go
+++ src/go/mongolib/proto/system.profile.go
@@ -178,7 +178,7 @@ func (self ExampleQuery) ExplainCmd() bson.D {
 					} else {
 						cmd = append(cmd[:i], cmd[i+1:]...)
 					}
-					break
+					// break LOOP
 				}
 			}
 		}
@@ -250,6 +250,8 @@ func (self ExampleQuery) ExplainCmd() bson.D {
 			}
 		}
 	case "command":
+		cmd = sanitizeCommand(cmd)
+
 		if len(cmd) == 0 || cmd[0].Key != "group" {
 			break
 		}
@@ -283,4 +285,29 @@ func (self ExampleQuery) ExplainCmd() bson.D {
 			Value: cmd,
 		},
 	}
+}
+
+func sanitizeCommand(cmd bson.D) bson.D {
+	if len(cmd) < 1 {
+		return cmd
+	}
+
+	key := cmd[0].Key
+	if key != "count" && key != "distinct" {
+		return cmd
+	}
+
+	for i := range cmd {
+		// drop $db param as it is not supported in MongoDB 3.0
+		if cmd[i].Key == "$db" {
+			if len(cmd)-1 == i {
+				cmd = cmd[:i]
+			} else {
+				cmd = append(cmd[:i], cmd[i+1:]...)
+			}
+			break
+		}
+	}
+
+	return cmd
 }
