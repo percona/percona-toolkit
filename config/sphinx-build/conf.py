04b70879dd2a98f20102604d04c5196cf5317ea5
Paul Jacobs <paul.jacobs@percona.com>
2021-01-13 17:08:31 +0300

    Update year

--- config/sphinx-build/conf.py
+++ config/sphinx-build/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Percona Toolkit'
-copyright = u'2020, Percona LLC and/or its affiliates'
+copyright = u'2021, Percona LLC and/or its affiliates'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -180,7 +180,7 @@ htmlhelp_basename = 'PerconaToolkitdoc'
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'PerconaToolkit.tex', u'Percona Toolkit Documentation',
-   u'2020, Percona LLC and/or its affiliates', 'manual'),
+   u'2021, Percona LLC and/or its affiliates', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,5 +214,5 @@ latex_toplevel_sectioning = 'part'
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'perconatoolkit', u'Percona Toolkit Documentation',
-     [u'2020, Percona LLC and/or its affiliates'], 1)
+     [u'2021, Percona LLC and/or its affiliates'], 1)
 ]
