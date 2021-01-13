b7fe50e7d35bf901f81127c6e53f02ef4518808a
Paul Jacobs <paul.jacobs@percona.com>
2021-01-13 17:12:11 +0300

    Correct path to icon

--- config/sphinx-build/conf.py
+++ config/sphinx-build/conf.py
@@ -116,7 +116,7 @@ html_short_title = 'Percona Toolkit'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'percona_favicon.ico'
+html_favicon = '_static/percona_favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
