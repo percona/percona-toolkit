8dff9b3026a947477c8733f2ec5b6c748357d4b6
Carlos Salguero <carlos.salguero@percona.com>
2021-11-01 14:26:57 -0300

    PT-2016 Added tests

---
+++ t/pt-table-sync/samples/pt-2016.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+
+CREATE TABLE test.test2 (
+  col1 INT NOT NULL,
+  col2 TEXT NOT NULL,
+  col3 VARCHAR(5) NOT NULL,
+  UNIQUE KEY (col1, col2(3))
+-- ,PRIMARY KEY (col1)
+);
+
+INSERT INTO test.test2 VALUES(1,'aaa','aaa');
