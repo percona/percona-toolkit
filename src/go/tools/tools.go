38f4857f19ead6593a4bb6713de24923ed423e17
Carlos Salguero <carlos.salguero@percona.com>
2021-11-03 10:12:24 -0300

    Updated makefile
    Updated makefile to:
    - Use ./bin/ directory to store tools like `gofumpt` or linters
    - Upgraded binaries
    - Added `go mod tidy` to `make format`
    - Updated ignored files list

---
+++ src/go/tools/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/reviewdog/reviewdog/cmd/reviewdog"
+	_ "golang.org/x/tools/cmd/goimports"
+)
