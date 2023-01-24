bbc50cab513c8d04961c8b44f8c008235f0d506e
svetasmirnova <sveta.smirnova@percona.com>
2023-01-24 21:21:11 +0300

    PT-2164 - pt-k8s-debug-collector does not have version flag
    Added flag
    Updated README

--- src/go/pt-k8s-debug-collector/main.go
+++ src/go/pt-k8s-debug-collector/main.go
@@ -4,8 +4,21 @@ import (
 	"flag"
 	"log"
 	"os"
+    "fmt"
 
 	"github.com/percona/percona-toolkit/src/go/pt-k8s-debug-collector/dumper"
+)
+
+const (
+    TOOLNAME = "pt-k8s-debug-collector"
+)
+
+// We do not set anything here, these variables are defined by the Makefile
+var (
+    Build     string
+    GoVersion string
+    Version   string
+    Commit    string
 )
 
 func main() {
@@ -14,13 +27,24 @@ func main() {
 	clusterName := ""
 	kubeconfig := ""
 	forwardport := ""
+    version := false
 
 	flag.StringVar(&namespace, "namespace", "", "Namespace for collecting data. If empty data will be collected from all namespaces")
 	flag.StringVar(&resource, "resource", "none", "Collect data, specific to the resource. Supported values: pxc, psmdb, pg, ps, none")
 	flag.StringVar(&clusterName, "cluster", "", "Cluster name")
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to kubeconfig")
 	flag.StringVar(&forwardport, "forwardport", "", "Port to use for  port forwarding")
+	flag.BoolVar(&version, "version", false, "Print version")
 	flag.Parse()
+
+    if (version) {
+        fmt.Println(TOOLNAME)
+        fmt.Printf("Version %s\n", Version)
+        fmt.Printf("Build: %s using %s\n", Build, GoVersion)
+        fmt.Printf("Commit: %s\n", Commit)
+
+        return
+    }
 
 	if len(clusterName) > 0 {
 		resource += "/" + clusterName
