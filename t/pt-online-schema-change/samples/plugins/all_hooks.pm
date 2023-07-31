8ece30ff2f7e293313272dd5b79818fb408533ee
Marcelo HP Ferreira <marcelohpf@gmail.com>
2023-07-31 15:30:24 +0200

    Add after nibble hook to pt-online-schema-change (#645)
    * Include hook after nibble on copy rows
    
    * Update tests with new hook for pt-online-schema-change

--- t/pt-online-schema-change/samples/plugins/all_hooks.pm
+++ t/pt-online-schema-change/samples/plugins/all_hooks.pm
@@ -51,6 +51,11 @@ sub before_copy_rows {
    print "PLUGIN before_copy_rows\n";
 }
  
+sub on_copy_rows_after_nibble {
+   my ($self, %args) = @_;
+   print "PLUGIN on_copy_rows_after_nibble\n";
+}
+ 
 sub after_copy_rows {
    my ($self, %args) = @_;
    print "PLUGIN after_copy_rows\n";
