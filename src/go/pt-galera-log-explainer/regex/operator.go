7513e1ded76887c1d4621f4bbe8658d5df9223b9
Yoann La Cancellera <lacancellera.yoann@gmail.com>
2023-10-18 23:40:36 +0200

    Add: operator member assocations regex
    It existed for non-operator setup, but was not working for operators due
    to k8s logs not interpreting newlines and tabs
    This operator version re-uses existing regular regex handlers directly

--- src/go/pt-galera-log-explainer/regex/operator.go
+++ src/go/pt-galera-log-explainer/regex/operator.go
@@ -47,9 +47,41 @@ var PXCOperatorMap = types.RegexMap{
 	"RegexGcacheScan": &types.LogRegex{
 		// those "operators" regexes do not have the log prefix added implicitely. It's not strictly needed, but
 		// it will help to avoid catching random piece of log out of order
-		Regex: regexp.MustCompile("^{\"log\":\".*GCache::RingBuffer initial scan"),
+		Regex: regexp.MustCompile(k8sprefix + ".*GCache::RingBuffer initial scan"),
 		Handler: func(submatches map[string]string, ctx types.LogCtx, log string) (types.LogCtx, types.LogDisplayer) {
 			return ctx, types.SimpleDisplayer("recovering gcache")
 		},
+	},
+
+	// Unusual regex: because operators log does not handle newlines, it is contracted in a single line
+	// which the common "IdentsMap" regexes will miss. Even if they would catch it, it would only catch a single one, not all info
+	// so this regex is about capturing subgroups to re-handle each them to the appropriate existing IdentsMap regex
+	"RegexOperatorMemberAssociations": &types.LogRegex{
+		Regex:         regexp.MustCompile("================================================.*View:"),
+		InternalRegex: regexp.MustCompile("own_index: " + regexIdx + ".*(?P<memberlog>" + IdentsMap["RegexMemberCount"].Regex.String() + ")(?P<compiledAssocations>(....-?[0-9]{1,2}(\\.-?[0-9])?: [a-z0-9]+-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]+, [a-zA-Z0-9-_\\.]+)+)"),
+		Handler: func(submatches map[string]string, ctx types.LogCtx, log string) (types.LogCtx, types.LogDisplayer) {
+
+			ctx.MyIdx = submatches[groupIdx]
+
+			var (
+				displayer types.LogDisplayer
+				msg       string
+			)
+
+			ctx, displayer = IdentsMap["RegexMemberCount"].Handle(ctx, submatches["memberlog"])
+			msg += displayer(ctx) + "; "
+
+			subAssociations := strings.Split(submatches["compiledAssocations"], "\\n\\t")
+			if len(subAssociations) < 2 {
+				return ctx, types.SimpleDisplayer(msg)
+			}
+			for _, subAssocation := range subAssociations[1:] {
+				// better to reuse the idents regex
+				ctx, displayer = IdentsMap["RegexMemberAssociations"].Handle(ctx, subAssocation)
+				msg += displayer(ctx) + "; "
+			}
+			return ctx, types.SimpleDisplayer(msg)
+		},
+		Verbosity: types.DebugMySQL,
 	},
 }
