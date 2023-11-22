304bc91b27f6d70bcee2efc62f666a4fedb8f5bf
Yoann La Cancellera <lacancellera.yoann@gmail.com>
2023-11-22 09:33:49 +0100

    Imp: use strings.builder for conflicts

--- src/go/pt-galera-log-explainer/main_test.go
+++ src/go/pt-galera-log-explainer/main_test.go
@@ -97,6 +97,12 @@ func TestMain(t *testing.T) {
 			cmd:  []string{"list", "--all", "--pxc-operator", "--no-color"},
 			path: "tests/logs/operator_ambiguous_ips/*",
 		},
+
+		{
+			name: "conflict_conflicts",
+			cmd:  []string{"conflicts"},
+			path: "tests/logs/conflict/*",
+		},
 	}
 
 TESTS:
