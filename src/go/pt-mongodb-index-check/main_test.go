cb6ccffe3bff8052a7a6d3e757225d26433e91f4
Vladyslav Yurchenko <silvia.homam@gmail.com>
2026-01-21 21:53:28 +0200

    add tests

--- src/go/pt-mongodb-index-check/main_test.go
+++ src/go/pt-mongodb-index-check/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os/exec"
 	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -18,5 +19,18 @@ func TestVersionOption(t *testing.T) {
 	re := regexp.MustCompile(toolname + `\n.*Version v?\d+\.\d+\.\d+\n`)
 	if !re.Match(out) {
 		t.Errorf("%s --version returns wrong result:\n%s", toolname, out)
+	}
+}
+
+func TestNoCommand(t *testing.T) {
+	mockMongo := "mongodb://127.0.0.1:27017"
+	out, err := exec.Command("../../../bin/"+toolname, "--mongodb.uri", mockMongo).Output()
+	if err != nil {
+		t.Errorf("error executing %s with no command: %s", toolname, err.Error())
+	}
+
+	want := "Usage: pt-mongodb-index-check show-help"
+	if !strings.Contains(string(out), want) {
+		t.Errorf("Output missmatch. Output %q should contain %q", string(out), want)
 	}
 }
