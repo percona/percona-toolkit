4c9d773c8871650f7b5d3279d19970408c27b51a
Vladyslav Yurchenko <silvia.homam@gmail.com>
2026-01-28 20:43:20 +0200

    PT-2368 - full refactor of a config library
    This refactor includes new config lib based on kong module.
    This lib is now follows an official percona specs, for parsing
    default and specified configs.

--- src/go/lib/config/config_test.go
+++ src/go/lib/config/config_test.go
@@ -1,73 +1,102 @@
 package config
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"os/user"
 	"path"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/alecthomas/kong"
 	"github.com/percona/percona-toolkit/src/go/lib/tutil"
 )
 
-func TestReadConfig(t *testing.T) {
+type KongFlags struct {
+	VersionCheck   bool    `name:"version-check" negatable:"" default:"true"`
+	TrueBoolVar    bool    `name:"trueboolvar" help:"test"`
+	YesBoolVar     BoolYN  `name:"yesboolvar" help:"test"`
+	FalseBoolVar   bool    `name:"falseboolvar" help:"test"`
+	NoBoolVar      BoolYN  `name:"noboolvar" help:"test"`
+	IntVar         int     `name:"intvar" default:"0"`
+	FloatVar       float64 `name:"floatvar" default:"0.0"`
+	StringVar      string  `name:"stringvar"`
+	NewString      string  `name:"newstring" short:"n"`
+	AnotherInt     int     `name:"anotherint" default:"0" short:"a"`
+	IgnoredComment string  `name:"ignoredcomment"`
+}
+
+func TestReadConfigKong(t *testing.T) {
 	rootPath, err := tutil.RootPath()
 	if err != nil {
 		t.Errorf("cannot get root path: %s", err)
 	}
 	file := path.Join(rootPath, "src/go/tests/lib/sample-config1.conf")
 
-	conf := NewConfig(file)
+	var mockArgs []string
 
-	keys := []string{"no-version-check", "trueboolvar", "yesboolvar", "noboolvar", "falseboolvar", "intvar", "floatvar", "stringvar"}
-	for _, key := range keys {
-		if !conf.HasKey(key) {
-			t.Errorf("missing %s key", key)
-		}
+	mockArgs = append(mockArgs, os.Args[0])
+
+	mockArgs = append(mockArgs, []string{"--config", file}...)
+	os.Args = mockArgs
+
+	f := &KongFlags{}
+	toolName := "pt-tools-config-test"
+
+	_, _, err = Setup(toolName, f)
+	if err != nil {
+		t.Error(err)
 	}
 
 	// no-version-check
-	if conf.GetBool("no-version-check") != true {
+	if f.VersionCheck {
 		t.Error("no-version-check should be enabled")
 	}
 
 	// trueboolvar=true
-	if conf.GetBool("trueboolvar") != true {
+	if !f.TrueBoolVar {
 		t.Error("trueboolvar should be true")
 	}
 
 	// yesboolvar=yes
-	if conf.GetBool("yesboolvar") != true {
+	if !f.YesBoolVar {
 		t.Error("yesboolvar should be true")
 	}
 
 	// falseboolvar=false
-	if conf.GetBool("falseboolvar") != false {
+	if f.FalseBoolVar {
 		t.Error("trueboolvar should be false")
 	}
 
 	// noboolvar=no
-	if conf.GetBool("noboolvar") != false {
+	if f.NoBoolVar {
 		t.Error("yesboolvar should be false")
 	}
 
 	// intvar=1
-	if got := conf.GetInt64("intvar"); got != 1 {
-		t.Errorf("intvar should be 1, got %d", got)
+	if f.IntVar != 1 {
+		t.Errorf("intvar should be 1, got %d", f.IntVar)
 	}
 
 	// floatvar=2.3
-	if got := conf.GetFloat64("floatvar"); got != 2.3 {
-		t.Errorf("floatvar should be 2.3, got %f", got)
+	if f.FloatVar != 2.3 {
+		t.Errorf("floatvar should be 2.3, got %f", f.FloatVar)
 	}
 
 	// stringvar=some string var having = and #
-	if got := conf.GetString("stringvar"); got != "some string var having = and #" {
-		t.Errorf("string var incorrect value; got %s", got)
+	if f.StringVar != "some string var having = and #" {
+		t.Errorf("string var incorrect value; got %q", f.StringVar)
+	}
+
+	if f.IgnoredComment != "" {
+		t.Errorf("ignoredcomment should be empty; got %q", f.IgnoredComment)
 	}
 }
 
-func TestOverrideConfig(t *testing.T) {
+func TestOverrideConfigKong(t *testing.T) {
 	rootPath, err := tutil.RootPath()
 	if err != nil {
 		t.Errorf("cannot get root path: %s", err)
@@ -75,60 +104,149 @@ func TestOverrideConfig(t *testing.T) {
 	file1 := path.Join(rootPath, "src/go/tests/lib/sample-config1.conf")
 	file2 := path.Join(rootPath, "src/go/tests/lib/sample-config2.conf")
 
-	conf := NewConfig(file1, file2)
+	var mockArgs []string
 
-	keys := []string{"no-version-check", "trueboolvar", "yesboolvar", "noboolvar", "falseboolvar", "intvar", "floatvar", "stringvar"}
-	for _, key := range keys {
-		if !conf.HasKey(key) {
-			t.Errorf("missing %s key", key)
-		}
+	mockArgs = append(mockArgs, os.Args[0])
+
+	mockArgs = append(mockArgs, []string{"--config", fmt.Sprintf("%s,%s", file1, file2)}...)
+	os.Args = mockArgs
+
+	f := &KongFlags{}
+	toolName := "pt-tools-config-test"
+
+	_, _, err = Setup(toolName, f)
+	if err != nil {
+		t.Error(err)
 	}
 
 	// no-version-check. This option is missing in the 2nd file.
 	// It should remain unchanged
-	if conf.GetBool("no-version-check") != true {
+	if f.VersionCheck {
 		t.Error("no-version-check should be enabled")
 	}
 
-	if conf.GetBool("trueboolvar") == true {
+	if f.TrueBoolVar {
 		t.Error("trueboolvar should be false")
 	}
 
-	if conf.GetBool("yesboolvar") == true {
+	if f.YesBoolVar {
 		t.Error("yesboolvar should be false")
 	}
 
-	if conf.GetBool("falseboolvar") == false {
+	if !f.FalseBoolVar {
 		t.Error("trueboolvar should be true")
 	}
 
-	if conf.GetBool("noboolvar") == false {
+	if !f.NoBoolVar {
 		t.Error("yesboolvar should be true")
 	}
 
-	if got := conf.GetInt64("intvar"); got != 4 {
-		t.Errorf("intvar should be 4, got %d", got)
+	if f.IntVar != 4 {
+		t.Errorf("intvar should be 4, got %d", f.IntVar)
 	}
 
-	if got := conf.GetFloat64("floatvar"); got != 5.6 {
-		t.Errorf("floatvar should be 5.6, got %f", got)
+	if f.FloatVar != 5.6 {
+		t.Errorf("floatvar should be 5.6, got %f", f.FloatVar)
 	}
 
-	if got := conf.GetString("stringvar"); got != "some other string" {
-		t.Errorf("string var incorrect value; got %s", got)
+	if f.StringVar != "some other string" {
+		t.Errorf("string var incorrect value; got %s", f.StringVar)
 	}
 
 	// This exists only in file2
-	if got := conf.GetString("newstring"); got != "a new string" {
-		t.Errorf("string var incorrect value; got %s", got)
+	if f.NewString != "a new string" {
+		t.Errorf("string var incorrect value; got %s", f.NewString)
 	}
 
-	if got := conf.GetInt64("anotherint"); got != 8 {
-		t.Errorf("intvar should be 8, got %d", got)
+	if f.AnotherInt != 8 {
+		t.Errorf("intvar should be 8, got %d", f.AnotherInt)
+	}
+
+	if f.IgnoredComment != "" {
+		t.Errorf("ignoredcomment should be empty; got %q", f.IgnoredComment)
 	}
 }
 
-func TestDefaultFiles(t *testing.T) {
+func TestOverrideCMDConfigKong(t *testing.T) {
+	rootPath, err := tutil.RootPath()
+	if err != nil {
+		t.Errorf("cannot get root path: %s", err)
+	}
+	file1 := path.Join(rootPath, "src/go/tests/lib/sample-config1.conf")
+
+	var mockArgs []string
+
+	mockArgs = append(mockArgs, os.Args[0])
+
+	mockArgs = append(mockArgs,
+		"--config", file1,
+		"--trueboolvar=false", // reset bool flag
+		"--yesboolvar", "no",
+		"--falseboolvar=true", // reset bool flag
+		"--noboolvar", "yes",
+		"--intvar", "1337",
+		"--floatvar", "1337.1",
+		"--stringvar", "hello",
+		"-n", "world", // test shorthand
+		"-a", "3", // test shorthand
+	)
+	os.Args = mockArgs
+
+	f := &KongFlags{}
+	toolName := "pt-tools-config-test"
+
+	_, _, err = Setup(toolName, f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if f.VersionCheck {
+		t.Error("no-version-check should be enabled")
+	}
+
+	if f.TrueBoolVar {
+		t.Error("trueboolvar should be false")
+	}
+
+	if f.YesBoolVar {
+		t.Error("yesboolvar should be false")
+	}
+
+	if !f.FalseBoolVar {
+		t.Error("trueboolvar should be true")
+	}
+
+	if !f.NoBoolVar {
+		t.Error("yesboolvar should be true")
+	}
+
+	if f.IntVar != 1337 {
+		t.Errorf("intvar should be 1337, got %d", f.IntVar)
+	}
+
+	if f.FloatVar != 1337.1 {
+		t.Errorf("floatvar should be 1337.1, got %f", f.FloatVar)
+	}
+
+	if f.StringVar != "hello" {
+		t.Errorf("string var incorrect value; got %s", f.StringVar)
+	}
+
+	// This exists only in file2
+	if f.NewString != "world" {
+		t.Errorf("string var incorrect value; got %s", f.NewString)
+	}
+
+	if f.AnotherInt != 3 {
+		t.Errorf("intvar should be 3, got %d", f.AnotherInt)
+	}
+
+	if f.IgnoredComment != "" {
+		t.Errorf("ignoredcomment should be empty; got %q", f.IgnoredComment)
+	}
+}
+
+func TestDefaultFilesKong(t *testing.T) {
 	current, _ := user.Current()
 	toolname := "pt-testing"
 
@@ -139,12 +257,289 @@ func TestDefaultFiles(t *testing.T) {
 		fmt.Sprintf("%s/.%s.conf", current.HomeDir, toolname),
 	}
 
-	got, err := DefaultConfigFiles(toolname)
-	if err != nil {
-		t.Errorf("cannot get default config files list: %s", err)
-	}
+	got := getDefaultPaths(toolname)
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %#v\nwant: %#v\n", got, want)
 	}
 }
+
+func TestNewPerconaResolver(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    map[string]any
+		wantErr bool
+	}{
+		{
+			name: "basic_options",
+			input: `# Comment
+variable=Threads_connected
+cycles=2
+verbose`,
+			want: map[string]any{
+				"variable": "Threads_connected",
+				"cycles":   "2",
+				"verbose":  "true",
+			},
+			wantErr: false,
+		},
+		{
+			name: "with_no_prefix",
+			input: `option=value
+no-optimize`,
+			want: map[string]any{
+				"option":   "value",
+				"optimize": "false",
+			},
+			wantErr: false,
+		},
+		{
+			name: "with_double_dash_prefix", // Not valid according to specs but should pass
+			input: `--host=localhost
+--port=3306`,
+			want: map[string]any{
+				"host": "localhost",
+				"port": "3306",
+			},
+			wantErr: false,
+		},
+		{
+			name:  "empty_lines_and_comments",
+			input: "\n# Comment\n\n# Another comment\n\noption=value\n\n",
+			want: map[string]any{
+				"option": "value",
+			},
+			wantErr: false,
+		},
+		{
+			name: "spaces_around_equals", // Not valid according to specs but should pass
+			input: `key = value
+another=test`,
+			want: map[string]any{
+				"key":     "value",
+				"another": "test",
+			},
+			wantErr: false,
+		},
+		{
+			name:    "invalid_empty_key",
+			input:   `=value`,
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reader := strings.NewReader(tt.input)
+			got, err := NewPerconaResolver(reader)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPerconaResolver() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			if !reflect.DeepEqual(got.values, tt.want) {
+				t.Errorf("NewPerconaResolver() values = %v, want %v", got.values, tt.want)
+			}
+		})
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	tests := []struct {
+		name            string
+		content         string
+		wantOptions     string
+		wantPassthrough []string
+		wantErr         bool
+	}{
+		{
+			name: "basic_config",
+			content: `variable=Threads_connected
+cycles=2`,
+			wantOptions: `variable=Threads_connected
+cycles=2
+`,
+			wantPassthrough: nil,
+			wantErr:         false,
+		},
+		{
+			name: "with_passthrough",
+			content: `variable=Threads_connected
+cycles=2
+--
+--user daniel
+--password secret`,
+			wantOptions: `variable=Threads_connected
+cycles=2
+`,
+			wantPassthrough: []string{"--user", "daniel", "--password", "secret"},
+			wantErr:         false,
+		},
+		{
+			name: "passthrough_with_comments",
+			content: `option=value
+--
+# This is a comment
+--user root
+# Another comment
+--host localhost`,
+			wantOptions: `option=value
+`,
+			wantPassthrough: []string{"--user", "root", "--host", "localhost"},
+			wantErr:         false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create temp file
+			tmpDir := t.TempDir()
+			tmpFile := filepath.Join(tmpDir, "test.conf")
+			if err := os.WriteFile(tmpFile, []byte(tt.content), 0644); err != nil {
+				t.Fatalf("Failed to create temp file: %v", err)
+			}
+
+			got, err := loadConfig(tmpFile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			// Check options
+			var buf bytes.Buffer
+			if _, err := buf.ReadFrom(got.options); err != nil {
+				t.Fatalf("Failed to read options: %v", err)
+			}
+			if buf.String() != tt.wantOptions {
+				t.Errorf("loadConfig() options = %q, want %q", buf.String(), tt.wantOptions)
+			}
+
+			// Check passthrough
+			if !reflect.DeepEqual(got.passthrough, tt.wantPassthrough) {
+				t.Errorf("loadConfig() passthrough = %v, want %v", got.passthrough, tt.wantPassthrough)
+			}
+		})
+	}
+}
+
+func TestParseConfigFlag(t *testing.T) {
+	tests := []struct {
+		name             string
+		args             []string
+		wantPaths        []string
+		wantSpecified    bool
+		wantRemainingLen int
+		wantErr          bool
+	}{
+		{
+			name:             "no_config_flag",
+			args:             []string{"--verbose", "--host=localhost"},
+			wantPaths:        nil,
+			wantSpecified:    false,
+			wantRemainingLen: 2,
+			wantErr:          false,
+		},
+		{
+			name:             "single_config",
+			args:             []string{"--config", "/path/to/config.conf", "--verbose"},
+			wantPaths:        []string{"/path/to/config.conf"},
+			wantSpecified:    true,
+			wantRemainingLen: 1,
+			wantErr:          false,
+		},
+		{
+			name:             "multiple_configs",
+			args:             []string{"--config", "/etc/config.conf,~/.config.conf", "--verbose"},
+			wantPaths:        []string{"/etc/config.conf", "~/.config.conf"},
+			wantSpecified:    true,
+			wantRemainingLen: 1,
+			wantErr:          false,
+		},
+		{
+			name:             "empty_config",
+			args:             []string{"--config", "''", "--verbose"},
+			wantPaths:        nil,
+			wantSpecified:    true,
+			wantRemainingLen: 1,
+			wantErr:          false,
+		},
+		{
+			name:      "config_with_equals",
+			args:      []string{"--config=/path/to/config.conf"},
+			wantPaths: nil,
+			wantErr:   true,
+		},
+		{
+			name:      "config_without_value",
+			args:      []string{"--config"},
+			wantPaths: nil,
+			wantErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotPaths, gotSpecified, gotRemaining, err := parseConfigFlag(tt.args)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseConfigFlag() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if tt.wantErr {
+				return
+			}
+
+			if !reflect.DeepEqual(gotPaths, tt.wantPaths) {
+				t.Errorf("parseConfigFlag() paths = %v, want %v", gotPaths, tt.wantPaths)
+			}
+
+			if gotSpecified != tt.wantSpecified {
+				t.Errorf("parseConfigFlag() specified = %v, want %v", gotSpecified, tt.wantSpecified)
+			}
+
+			if len(gotRemaining) != tt.wantRemainingLen {
+				t.Errorf("parseConfigFlag() remaining len = %d, want %d", len(gotRemaining), tt.wantRemainingLen)
+			}
+		})
+	}
+}
+
+type mockScanner struct {
+	value    string
+	hasValue bool
+}
+
+func (m *mockScanner) Len() int {
+	if m.hasValue {
+		return 1
+	}
+	return 0
+}
+
+func (m *mockScanner) PopValueInto(name string, target interface{}) error {
+	if !m.hasValue {
+		return fmt.Errorf("no value set")
+	}
+	if s, ok := target.(*string); ok {
+		*s = m.value
+		return nil
+	}
+	return fmt.Errorf("scanner error")
+}
+
+// Additional mock methods required by kong.Scanner interface
+func (m *mockScanner) Pop() *kong.Token                   { return nil }
+func (m *mockScanner) Peek() *kong.Token                  { return nil }
+func (m *mockScanner) PushTyped(interface{}, *kong.Token) {}
--- src/go/pt-k8s-debug-collector/main.go
+++ src/go/pt-k8s-debug-collector/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/percona/percona-toolkit/src/go/lib/config"
+	"github.com/percona/percona-toolkit/src/go/lib/versioncheck"
 	"github.com/percona/percona-toolkit/src/go/pt-k8s-debug-collector/dumper"
 )
 
@@ -21,41 +22,57 @@ var (
 	Commit    string //nolint
 )
 
-func main() {
-	namespace := ""
-	resource := ""
-	clusterName := ""
-	kubeconfig := ""
-	forwardport := ""
-	version := false
-	skipPodSummary := false
+type cliOptions struct {
+	Namespace      string `name:"namespace" help:"Namespace for collecting data. If empty data will be collected from all namespaces"`
+	Resource       string `name:"resource" help:"Collect data, specific to the resource. Supported values: pxc, psmdb, pg, pgv2, ps, none, auto" default:"auto"`
+	ClusterName    string `name:"cluster" help:"Cluster name"`
+	Kubeconfig     string `name:"kubeconfig" help:"Path to kubeconfig"`
+	ForwardPort    string `name:"forwardport" help:"Port to use for  port forwarding"`
+	SkipPodSummary bool   `name:"skip-pod-summary" help:"Skip pod summary collection"`
+	Version        bool   `name:"version"`
+	VersionCheck   bool   `name:"version-check" negatable:"" default:"true"`
+}
 
-	flag.StringVar(&namespace, "namespace", "", "Namespace for collecting data. If empty data will be collected from all namespaces")
-	flag.StringVar(&resource, "resource", "auto", "Collect data, specific to the resource. Supported values: pxc, psmdb, pg, pgv2, ps, none, auto")
-	flag.StringVar(&clusterName, "cluster", "", "Cluster name")
-	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to kubeconfig")
-	flag.StringVar(&forwardport, "forwardport", "", "Port to use for  port forwarding")
-	flag.BoolVar(&version, "version", false, "Print version")
-	flag.BoolVar(&skipPodSummary, "skip-pod-summary", false, "Skip pod summary collection")
-	flag.Parse()
-
-	if version {
+func (c *cliOptions) AfterApply(args ...any) error {
+	if c.Version {
 		fmt.Println(toolname)
 		fmt.Printf("Version %s\n", Version)
 		fmt.Printf("Build: %s using %s\n", Build, GoVersion)
 		fmt.Printf("Commit: %s\n", Commit)
+		return nil
+	}
 
+	if len(c.ClusterName) > 0 {
+		c.Resource += "/" + c.ClusterName
+	}
+
+	if c.VersionCheck {
+		advice, err := versioncheck.CheckUpdates(toolname, Version)
+		if err != nil {
+			log.Printf("cannot check version updates: %s", err.Error())
+		} else if advice != "" {
+			log.Printf("%s", advice)
+		}
+	}
+	return nil
+}
+
+func main() {
+	opts := &cliOptions{}
+	_, _, err := config.Setup(toolname, opts)
+	if err != nil {
+		log.Printf("cannot get parameters: %s", err.Error())
+		os.Exit(1)
+	}
+
+	if opts.Version {
 		return
 	}
 
-	if len(clusterName) > 0 {
-		resource += "/" + clusterName
-	}
-
-	d := dumper.New("", namespace, resource, kubeconfig, forwardport, skipPodSummary)
+	d := dumper.New("", opts.Namespace, opts.Resource, opts.Kubeconfig, opts.ForwardPort, opts.SkipPodSummary)
 	log.Println("Start collecting cluster data")
 
-	err := d.DumpCluster()
+	err = d.DumpCluster()
 	if err != nil {
 		log.Println("Error:", err)
 		os.Exit(1)
