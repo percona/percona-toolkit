4a471e16c616ceb029e17680be199a25c558eec6
xmj147220591 <42802468+xmj147220591@users.noreply.github.com>
2026-03-06 10:36:27 +0800

    Update t/pt-archiver/samples/bulk_insert_special_chars.sql
    Co-authored-by: Sveta Smirnova <svetasmirnova@users.noreply.github.com>

--- t/pt-archiver/samples/bulk_insert_special_chars.sql
+++ t/pt-archiver/samples/bulk_insert_special_chars.sql
@@ -4,7 +4,7 @@ DROP DATABASE IF EXISTS `bulk_escape`;
 CREATE DATABASE `bulk_escape`;
 
 CREATE TABLE `bulk_escape`.`source` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL,
   `name` varchar(500) DEFAULT NULL,
   `job` varchar(450) DEFAULT NULL,
   `stu_id` int DEFAULT NULL,
